4a2f1797aada428477b117aa5523c44263822d02
schemak@pulipinc.com <schemak@pulipinc.com>
2021-01-26 16:23:25 +0900

    init

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -3,12 +3,12 @@ import 'firebase/auth'
 
 // The configuration below is not sensitive data. You can serenely add your config here
 const config = {
-  apiKey: 'AIzaSyCe6CSzOUs6_Ij_VHa6hancdEi6CNBtw7c',
+  apiKey: 'AIzaSyAlqvqm8G1r-9GhT-21ilYGYTJG0uI50aA',
   authDomain: 'bento-starter.firebaseapp.com',
-  databaseURL: 'https://bento-starter.firebaseio.com',
-  projectId: 'bento-starter',
-  storageBucket: 'bento-starter.appspot.com',
-  messagingSenderId: '458464977217'
+  databaseURL: '',
+  projectId: 'new-pulipin',
+  storageBucket: '',
+  messagingSenderId: ''
 }
 
 firebase.initializeApp(config)
